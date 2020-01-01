8a2025fc2cafc7efd0acb38b0f7dba77e05c984e
jennyshih <jinghua.shih@gmail.com>
2020-01-01 19:24:43 +0800

    day_06 part 2 completed

--- day_06.exs
+++ day_06.exs
@@ -9,19 +9,41 @@ defmodule Orbit do
 
   def count_total_orbits(data_map) do
     data_map
-    |> Stream.map(fn {_, v} -> count_individual_orbits(data_map, v) end)
+    |> Stream.map(fn {k, _} -> fetch_individual_orbit_chain_for(k, data_map) end)
+    |> Stream.map(&length/1)
     |> Enum.sum()
   end
 
-  def count_individual_orbits(data_map, node, count \\ 1) do
-    case data_map[node] do
-      nil -> count
-      _ -> count_individual_orbits(data_map, data_map[node], count + 1)
+  def count_orbital_transfers_between(obj1, obj2, data_map) do
+    count_mapset_differences = fn [set1, set2] ->
+      MapSet.difference(MapSet.union(set1, set2), MapSet.intersection(set1, set2))
+      |> MapSet.size()
+    end
+
+    [obj1, obj2]
+    |> Enum.map(&fetch_individual_orbit_chain_for(&1, data_map))
+    |> Enum.map(&MapSet.new/1)
+    |> count_mapset_differences.()
+  end
+
+  def fetch_individual_orbit_chain_for(node, data_map, chain \\ []) do
+    parent_node = data_map[node]
+
+    case parent_node do
+      nil -> chain
+      _ -> fetch_individual_orbit_chain_for(parent_node, data_map, [parent_node | chain])
     end
   end
 end
 
+# Part 1
 'input/day_06.txt'
 |> Orbit.draw_map_from_input()
 |> Orbit.count_total_orbits()
+|> IO.puts()
+
+# Part2
+'input/day_06.txt'
+|> Orbit.draw_map_from_input()
+|> (fn data -> Orbit.count_orbital_transfers_between("YOU", "SAN", data) end).()
 |> IO.puts()
