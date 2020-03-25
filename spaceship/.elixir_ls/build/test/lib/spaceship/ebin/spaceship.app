1d2183e8580aae0ce466140639b36b513df6b9b1
jennyshih <jinghua.shih@gmail.com>
2020-03-25 22:49:42 +0800

    added Registry module (wrapper for GenServer) for Spaceship

--- spaceship/.elixir_ls/build/test/lib/spaceship/ebin/spaceship.app
+++ spaceship/.elixir_ls/build/test/lib/spaceship/ebin/spaceship.app
@@ -1,6 +1,7 @@
 {application,spaceship,
              [{applications,[kernel,stdlib,elixir,logger]},
               {description,"spaceship"},
-              {modules,['Elixir.Spaceship']},
+              {modules,['Elixir.Spaceship','Elixir.Spaceship.Bucket',
+                        'Elixir.Spaceship.Registry']},
               {registered,[]},
               {vsn,"0.1.0"}]}.
---
+++ spaceship/test/registry_test.exs
@@ -0,0 +1,24 @@
+defmodule Spaceship.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    # Instead of using `{:ok, registry} = Spaceship.Registry.start_link([])`,
+    # in test we use the syntax below to ensure the state is properly cleaned up
+    # after each test.
+    registry = start_supervised!(Spaceship.Registry)
+    %{registry: registry}
+  end
+
+  test 'successful spawning buckets returns ok', %{registry: registry} do
+    assert :ok == Spaceship.Registry.create(registry, 'a bucket')
+  end
+
+  test 'lookup with inexistent name returns error', %{registry: registry} do
+    assert :error == Spaceship.Registry.lookup(registry, 'nonexistent bucket')
+  end
+
+  test 'successful lookup returns desired bucket', %{registry: registry} do
+    Spaceship.Registry.create(registry, 'a bucket')
+    assert {:ok, _bucket} = Spaceship.Registry.lookup(registry, 'a bucket')
+  end
+end
