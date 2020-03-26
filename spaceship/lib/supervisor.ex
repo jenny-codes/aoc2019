56c00e3e2c7b3d5c427a3b74855f4ac5f2989fa8
jennyshih <jinghua.shih@gmail.com>
2020-03-26 22:00:38 +0800

    added Supervisor

---
+++ spaceship/lib/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule Spaceship.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      {Spaceship.Registry, name: Spaceship.Registry}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
