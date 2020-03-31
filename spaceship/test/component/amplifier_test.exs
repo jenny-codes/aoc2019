4e55c12a8646264901c0e750635d1ac2932d7cba
jennyshih <jinghua.shih@gmail.com>
2020-03-31 21:48:39 +0800

    using macro decribe() to organize amplifier test cases

--- spaceship/test/component/amplifier_test.exs
+++ spaceship/test/component/amplifier_test.exs
@@ -1,48 +1,52 @@
 defmodule Spaceship.Component.AmplifierTest do
   use ExUnit.Case, async: true
 
-  test "#check_signal returns a signal if inbox is not empty" do
-    {:ok, amp} = Spaceship.Component.Amplifier.start_link(inbox: ["a signal"])
+  describe "check_signal/1" do
+    test "returns a signal if inbox is not empty" do
+      {:ok, amp} = Spaceship.Component.Amplifier.start_link(inbox: ["a signal"])
 
-    signal = Spaceship.Component.Amplifier.check_signal(amp)
-    assert signal == "a signal"
+      signal = Spaceship.Component.Amplifier.check_signal(amp)
+      assert signal == "a signal"
+    end
+
+    test "returns no_signal if inbox is empty" do
+      {:ok, amp} = Spaceship.Component.Amplifier.start_link()
+
+      signal = Spaceship.Component.Amplifier.check_signal(amp)
+      assert signal == :no_signal
+    end
+
+    test "removes singnals from the head of list" do
+      {:ok, amp} = Spaceship.Component.Amplifier.start_link(inbox: ["signal1", "signal2"])
+
+      signal1 = Spaceship.Component.Amplifier.check_signal(amp)
+      signal2 = Spaceship.Component.Amplifier.check_signal(amp)
+
+      assert signal1 == "signal1"
+      assert signal2 == "signal2"
+    end
   end
 
-  test "#check_signal returns no_signal if inbox is empty" do
-    {:ok, amp} = Spaceship.Component.Amplifier.start_link()
+  describe "send_signal/2" do
+    test "stores a signal in the inbox" do
+      {:ok, amp} = Spaceship.Component.Amplifier.start_link()
 
-    signal = Spaceship.Component.Amplifier.check_signal(amp)
-    assert signal == :no_signal
-  end
+      Spaceship.Component.Amplifier.send_signal(amp, "a signal")
+      signal = Spaceship.Component.Amplifier.check_signal(amp)
 
-  test "#check_signal removes singnals from the head of list" do
-    {:ok, amp} = Spaceship.Component.Amplifier.start_link(inbox: ["signal1", "signal2"])
+      assert signal == "a signal"
+    end
 
-    signal1 = Spaceship.Component.Amplifier.check_signal(amp)
-    signal2 = Spaceship.Component.Amplifier.check_signal(amp)
+    test "stores signals in the right order" do
+      {:ok, amp} = Spaceship.Component.Amplifier.start_link()
+      Spaceship.Component.Amplifier.send_signal(amp, "signal 1")
+      Spaceship.Component.Amplifier.send_signal(amp, "signal 2")
 
-    assert signal1 == "signal1"
-    assert signal2 == "signal2"
-  end
+      signal1 = Spaceship.Component.Amplifier.check_signal(amp)
+      signal2 = Spaceship.Component.Amplifier.check_signal(amp)
 
-  test "#send_signal stores a signal in the inbox" do
-    {:ok, amp} = Spaceship.Component.Amplifier.start_link()
-
-    Spaceship.Component.Amplifier.send_signal(amp, "a signal")
-    signal = Spaceship.Component.Amplifier.check_signal(amp)
-
-    assert signal == "a signal"
-  end
-
-  test "#send_signal stores signals in the right order" do
-    {:ok, amp} = Spaceship.Component.Amplifier.start_link()
-    Spaceship.Component.Amplifier.send_signal(amp, "signal 1")
-    Spaceship.Component.Amplifier.send_signal(amp, "signal 2")
-
-    signal1 = Spaceship.Component.Amplifier.check_signal(amp)
-    signal2 = Spaceship.Component.Amplifier.check_signal(amp)
-
-    assert signal1 == "signal 1"
-    assert signal2 == "signal 2"
+      assert signal1 == "signal 1"
+      assert signal2 == "signal 2"
+    end
   end
 end
