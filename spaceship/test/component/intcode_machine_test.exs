e6bd956b125b0e85be8e3c2421b8c38ba96c9447
jennyshih <jinghua.shih@gmail.com>
2020-04-05 08:01:08 +0800

    refactored IntcodeMachineTest for organization and readability

--- spaceship/test/component/intcode_machine_test.exs
+++ spaceship/test/component/intcode_machine_test.exs
@@ -15,111 +15,113 @@ defmodule Spaceship.Component.IntcodeMachineTest do
     assert Spaceship.Component.IntcodeMachine.deconstruct(program) == expected_progrma_str
   end
 
-  test 'opcode 1 adds the 1st and 2nd param and update to the location 3rd param points to' do
-    {program, next_pos} = execute_intcode_machine("1101,100,200,3")
+  describe "opcodes function in immediate mode" do
+    test "opcode 1 adds the 1st and 2nd param and update to the location 3rd param points to" do
+      {program, next_pos} = execute_intcode_machine("1101,100,200,3")
 
-    assert next_pos == 4
-    assert program == "1101,100,200,300"
-  end
-
-  test 'opcode 2 times the 1st and 2nd param and update to the location 3rd param points to' do
-    {program, next_pos} = execute_intcode_machine("1102,100,200,3")
-
-    assert next_pos == 4
-    assert program == "1102,100,200,20000"
-  end
-
-  test 'opcode 3 gets input from :input_fn and updates to the location 1st param points to' do
-    input_fn = fn opts ->
-      [input_val | updated_input_args] = opts[:input_args]
-      {input_val, Keyword.put(opts, :input_args, updated_input_args)}
+      assert next_pos == 4
+      assert program == "1101,100,200,300"
     end
 
-    {program, next_pos} =
-      execute_intcode_machine("1103,0", input_fn: input_fn, input_args: [10000])
+    test "opcode 2 times the 1st and 2nd param and update to the location 3rd param points to" do
+      {program, next_pos} = execute_intcode_machine("1102,100,200,3")
 
-    assert next_pos == 2
-    assert program == "10000,0"
-  end
+      assert next_pos == 4
+      assert program == "1102,100,200,20000"
+    end
 
-  test 'opcode 4 gets instruction from :output_fn with the value of 1st param' do
-    output_fn = fn(_val) -> :return end
+    test "opcode 3 gets input from :input_fn and updates to the location 1st param points to" do
+      input_fn = fn opts ->
+        [input_val | updated_input_args] = opts[:input_args]
+        {input_val, Keyword.put(opts, :input_args, updated_input_args)}
+      end
 
-    ret_value =
-      "1104,10000"
+      {program, next_pos} =
+      execute_intcode_machine("103,0", input_fn: input_fn, input_args: [10000])
+
+      assert next_pos == 2
+      assert program == "10000,0"
+    end
+
+    test "opcode 4 gets instruction from :output_fn with the value of 1st param" do
+      output_fn = fn _val -> :return end
+
+      ret_value =
+      "104,10000"
       |> Spaceship.Component.IntcodeMachine.build_program()
       |> Spaceship.Component.IntcodeMachine.execute(output_fn: output_fn)
 
-    assert ret_value == 10000
-  end
+      assert ret_value == 10000
+    end
 
-  test 'opcode 5 moves forward if 1st param is 0' do
-    {program, next_pos} = execute_intcode_machine("1105,0,100")
+    test "opcode 5 moves forward if 1st param is 0" do
+      {program, next_pos} = execute_intcode_machine("1105,0,100")
 
-    assert program == "1105,0,100"
-    assert next_pos == 3
-  end
+      assert program == "1105,0,100"
+      assert next_pos == 3
+    end
 
-  test 'opcode 5 jumps to the location of 2nd param if 1st param is not 0' do
-    {program, next_pos} = execute_intcode_machine("1105,1,100")
+    test "opcode 5 jumps to the location of 2nd param if 1st param is not 0" do
+      {program, next_pos} = execute_intcode_machine("1105,1,100")
 
-    assert program == "1105,1,100"
-    assert next_pos == 100
-  end
+      assert program == "1105,1,100"
+      assert next_pos == 100
+    end
 
-  test 'opcode 6 moves forward if 1st param is not 0' do
-    {program, next_pos} = execute_intcode_machine("1106,1,100")
+    test "opcode 6 moves forward if 1st param is not 0" do
+      {program, next_pos} = execute_intcode_machine("1106,1,100")
 
-    assert program == "1106,1,100"
-    assert next_pos == 3
-  end
+      assert program == "1106,1,100"
+      assert next_pos == 3
+    end
 
-  test 'opcode 6 jumps to the location of 2nd param if 1st param is 0' do
-    {program, next_pos} = execute_intcode_machine("1106,0,100")
+    test "opcode 6 jumps to the location of 2nd param if 1st param is 0" do
+      {program, next_pos} = execute_intcode_machine("1106,0,100")
 
-    assert program == "1106,0,100"
-    assert next_pos == 100
-  end
+      assert program == "1106,0,100"
+      assert next_pos == 100
+    end
 
-  test 'opcode 7 stores 1 in the position given by 3rd param if 1st param < 2nd param' do
-    {program, next_pos} = execute_intcode_machine("1107,99,100,0")
+    test "opcode 7 stores 1 in the position given by 3rd param if 1st param < 2nd param" do
+      {program, next_pos} = execute_intcode_machine("1107,99,100,0")
 
-    assert program == "1,99,100,0"
-    assert next_pos == 4
-  end
+      assert program == "1,99,100,0"
+      assert next_pos == 4
+    end
 
-  test 'opcode 7 stores 0 in the position given by 3rd param if 1st param >= 2nd param' do
-    {program, next_pos} = execute_intcode_machine("1107,101,100,0")
+    test "opcode 7 stores 0 in the position given by 3rd param if 1st param >= 2nd param" do
+      {program, next_pos} = execute_intcode_machine("1107,101,100,0")
 
-    assert program == "0,101,100,0"
-    assert next_pos == 4
-  end
+      assert program == "0,101,100,0"
+      assert next_pos == 4
+    end
 
-  test 'opcode 8 stores 0 in the position given by 3rd param if 1st param == 2nd param' do
-    {program, next_pos} = execute_intcode_machine("1108,100,100,0")
+    test "opcode 8 stores 0 in the position given by 3rd param if 1st param == 2nd param" do
+      {program, next_pos} = execute_intcode_machine("1108,100,100,0")
 
-    assert program == "1,100,100,0"
-    assert next_pos == 4
-  end
+      assert program == "1,100,100,0"
+      assert next_pos == 4
+    end
 
-  test 'opcode 8 stores 0 in the position given by 3rd param if 1st param != 2nd param' do
-    {program, next_pos} = execute_intcode_machine("1108,99,100,0")
+    test "opcode 8 stores 0 in the position given by 3rd param if 1st param != 2nd param" do
+      {program, next_pos} = execute_intcode_machine("1108,99,100,0")
 
-    assert program == "0,99,100,0"
-    assert next_pos == 4
-  end
+      assert program == "0,99,100,0"
+      assert next_pos == 4
+    end
 
-  test 'opcode 9 updates :relative_base with 1st param' do
-    {_program, next_pos, opts} =
+    test "opcode 9 updates :relative_base with 1st param" do
+      {_program, next_pos, opts} =
       "109,333"
       |> Spaceship.Component.IntcodeMachine.build_program()
       |> Spaceship.Component.IntcodeMachine.execute(is_debug: true)
 
-    assert next_pos == 2
-    assert opts[:relative_base] == 333
+      assert next_pos == 2
+      assert opts[:relative_base] == 333
+    end
   end
 
-  test 'opcode 99 returns :ok' do
+  test "opcode 99 returns :ok" do
     ret_value =
       "99"
       |> Spaceship.Component.IntcodeMachine.build_program()
